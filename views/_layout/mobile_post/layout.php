085d8cf264648901fef5ce406d37b305ed63ce47
boyoon8 <boyoon8@example.com>
2018-06-20 13:21:25 +0900

    b

--- views/_layout/mobile_post/layout.php
+++ views/_layout/mobile_post/layout.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="agd-partner-manual-verification" />
 <title><?php echo html_escape(element('page_title', $layout)); ?></title>
 <link rel="icon" href="<?php echo base_url('assets/images/temp/favi02.png'); ?>"/> 
 <?php if (element('meta_description', $layout)) { ?><meta name="description" content="<?php echo html_escape(element('meta_description', $layout)); ?>"><?php } ?>
